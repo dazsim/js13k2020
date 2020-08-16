3c891e7dc570e4761a9bc9cc311803e1fe051921
workshopcraft <darrensim42@gmail.com>
2020-08-16 08:42:54 +0100

    framework commit

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-js13k2020
+# js13kgames-parcel-starter
+This repo uses [Parcel](https://parceljs.org/) to build and zip games created for [js13kgames](http://js13kgames.com/).
+
+## Requirements
+
+The commands assume [Yarn](https://yarnpkg.com/en/docs/install) is installed.
+
+## Commands
+
+### `yarn start`
+
+Start the Parcel build server at `http://localhost:1234`.
+
+### `yarn build`
+
+Build, minify, and inline the game to `./dist/inlined/index.html`.
+
+### `yarn party`
+
+Build, minify, inline, and zip the game to `./dist/zipped/game.zip`. This command finishes with a log message that says if the zip file is under 13k.
---
+++ src/index.js
@@ -0,0 +1,51 @@
+const width = 640;
+const height = 480;
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d");
+let size = { width: 50, height: 50 };
+let position = { x: 0, y: 0 };
+let velocity = { x: 1, y: 1 };
+let states = [
+  "home",
+  "about",
+  "settings",
+  "save",
+  "load",
+  "new",
+  "open",
+  "brewing",
+  "talking",
+  "disecting",
+  
+
+]
+let menu = [
+  "home","about","settings"
+]
+
+canvas.width = width;
+canvas.height = height;
+
+function draw() {
+  requestAnimationFrame(draw);
+  ctx.clearRect(0, 0, width, height);
+
+  position.x += velocity.x;
+  position.y += velocity.y;
+
+  if (position.x + size.width > width || position.x < 0) {
+    velocity.x = -velocity.x;
+  }
+
+  if (position.y + size.height > height || position.y < 0) {
+    velocity.y = -velocity.y;
+  }
+
+  ctx.fillStyle = "rgb(200, 0, 0)";
+  ctx.fillRect(position.x, position.y, size.width, size.height);
+
+  ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
+  ctx.fillRect(30, 30, 50, 50);
+}
+
+draw();
